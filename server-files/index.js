c81c7ba80aa60b7cde859db40527b76c9af5aebe
Nic Bonetto <nick.bonetto@gmail.com>
2017-08-02 14:06:36 -0700

    Added POST route to create new note. #3

---
+++ server-files/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import { getNotes, postNote } from './knex'
+
+const app = express()
+
+app.use(express.static(__dirname + '/public/'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.get('/notes', (req, res) => {
+  getNotes()
+    .then(response => {
+      res.json(response)
+    })
+})
+
+app.post('/create-note', (req, res) => {
+  postNote(req.body.note)
+    .then(() => res.sendStatus(201))
+})
+
+app.listen(3000)
---
+++ server-files/knex.js
@@ -0,0 +1,23 @@
+import Knex from 'knex'
+
+const knex = Knex({
+  dialect: 'pg',
+  connection: 'postegres://localhost:5432/notes'
+})
+
+export function getNotes() {
+  const query = knex
+    .table('notes')
+    .select('note_content', 'id')
+    .orderBy('id', 'asc')
+
+  return query
+}
+
+export function postNote(newNote) {
+  const query = knex
+    .insert({ note_content: newNote })
+    .into('notes')
+
+  return query
+}
